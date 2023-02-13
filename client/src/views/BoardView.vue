2d81127c35c272bee4981f5e5c7db65cc4d1fdbf
Mark Venuto <markvenutobass@gmail.com>
2023-02-13 16:43:22 -0500

    implemented undo/redo

--- client/src/views/BoardView.vue
+++ client/src/views/BoardView.vue
@@ -1,19 +1,117 @@
 <script setup lang="ts">
-import Board from '../components/Board.vue';
+import { onMounted, ref, type Ref } from 'vue';
 
-import { ref, type Ref } from 'vue';
 
 const brushColor = ref('#000000');
 const brushWidth = ref(7)
-const BoardRef = ref<InstanceType<typeof Board> | null>(null)
+
+let mousePressed = false;
+let lastX: number, lastY: number;
+let canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D;
+
+function initBoard() {
+  canvas = document.getElementById('board') as HTMLCanvasElement;
+  ctx = canvas.getContext('2d')!;
+
+  // specify width and height of canvas based on wrapper div
+  let sketch = document.querySelector('#sketch');
+  let sketch_style = getComputedStyle(sketch as Element);
+  canvas.width = parseInt(sketch_style.getPropertyValue('width'));
+  canvas.height = parseInt(sketch_style.getPropertyValue('height'));
+
+  canvas.addEventListener("mousedown", function (e) {
+    mousePressed = true;
+    draw(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, false);
+  });
+
+  canvas.addEventListener("mousemove", function (e) {
+    if (mousePressed) {
+      draw(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
+    }
+  });
+
+  canvas.addEventListener("mouseup", function (e) {
+    if (mousePressed) {
+      mousePressed = false;
+      push();
+    }
+  });
+
+  canvas.addEventListener("mouseleave", function (e) {
+    if (mousePressed) {
+      mousePressed = false;
+      push();
+    }
+  });
+  // drawImage();
+  push();
+}
+
+// function drawImage() {
+//     let image = new Image();
+//     image.src = 'myimg.jpg';
+//     image.onload = (function () {
+//         ctx.drawImage(image, 0, 0, 500, 200);
+//         push();
+//     }); 
+// }
+
+function draw(x: number, y: number, isDown: boolean) {
+  if (isDown) {
+    ctx.beginPath();
+    ctx.strokeStyle = brushColor.value;
+    ctx.lineWidth = brushWidth.value;
+    ctx.lineJoin = "round";
+    ctx.moveTo(lastX, lastY);
+    ctx.lineTo(x, y);
+    ctx.closePath();
+    ctx.stroke();
+  }
+  lastX = x; lastY = y;
+}
 
 function clear() {
-  BoardRef.value?.clear();
+  ctx.setTransform(1, 0, 0, 1, 0, 0);
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+}
+
+let cPushArray = new Array();
+let cStep = -1;
+
+function push() {
+  cStep++;
+  if (cStep < cPushArray.length) { cPushArray.length = cStep; }
+  cPushArray.push(canvas.toDataURL());
 }
 
 function undo() {
-  BoardRef.value?.undo();
+  if (cStep > 0) {
+    cStep--;
+    let canvasPic = new Image();
+    canvasPic.src = cPushArray[cStep];
+    canvasPic.onload = function () {
+      clear(); 
+      ctx.drawImage(canvasPic, 0, 0); 
+    }
+  }
 }
+
+function redo() {
+  if (cStep < cPushArray.length - 1) {
+    cStep++;
+    let canvasPic = new Image();
+    canvasPic.onload = function () { 
+      clear(); 
+      ctx.drawImage(canvasPic, 0, 0); 
+    }
+    canvasPic.src = cPushArray[cStep];
+  }
+
+}
+
+onMounted(() => {
+  initBoard();
+})
 
 </script>
 
@@ -24,12 +122,15 @@ function undo() {
       <div id="button-container">
         <button @click="clear">Clear</button>
         <button @click="undo">Undo</button>
+        <button @click="redo">Redo</button>
       </div>
       <div id="slider-container">
         <input type="range" min="3" max="11" v-model.number="brushWidth" class="slider" id="myRange">
       </div>
     </div>
-    <Board :brushColor="brushColor" :brushWidth="brushWidth" ref="BoardRef" />
+    <div class="sketch" id="sketch">
+      <canvas class="board" id="board"></canvas>
+    </div>
   </div>
 </template>
 
@@ -52,5 +153,11 @@ function undo() {
   border-bottom: 1px solid white;
   display: flex;
   justify-content: center;
+}
+
+.sketch {
+  background-color: white;
+  width: 758px;
+  height: 424px;
 }
 </style>
